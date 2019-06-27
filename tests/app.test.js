4f5fd91d01e29eebbb746a25b656f6a2be6564e9
Zach Butler <zach.standley93@gmail.com>
2019-06-27 08:51:23 -0700

    changes

---
+++ tests/app.test.js
@@ -0,0 +1,12 @@
+// const request = require('supertest');
+// const app = require('../lib/app');
+
+// describe('application route', () => {
+//     it('returns an index.html file', () => {
+//         return request(app)
+//             .get('/index.html')
+//             .then(res => {
+//                 expect(res.text).toEqual(expect.stringContaining('you found me'));
+//             });
+//     }); 
+// });
